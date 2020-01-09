df95f543cae3879b994d2f689163bf50bcd9519e
lemmusm <lemmusm@gmail.com>
2020-01-09 12:02:35 -0600

    S traen los datos a traves degetInitialProps()

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,117 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React, { Component } from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+// Firebase
+import loadFirebase from '../firebase.config';
 
-    <Nav />
+class Home extends Component {
+  static async getInitialProps() {
+    const firebase = await loadFirebase();
+    const db = firebase.firestore();
+    let result = await new Promise((resolve, reject) => {
+      db.collection('nextjs-fb')
+        .get()
+        .then(snapshot => {
+          let data = [];
+          snapshot.forEach(doc => {
+            data.push(
+              Object.assign(
+                {
+                  id: doc.id
+                },
+                doc.data()
+              )
+            );
+          });
+          resolve(data);
+        })
+        .catch(error => {
+          reject([]);
+        });
+    });
+    return { usuarios: result };
+  }
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+  render() {
+    const usuarios = this.props.usuarios;
+    return (
+      <div>
+        <Head>
+          <title>Home</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+        <Nav />
+
+        <div className='hero'>
+          <h1 className='title'>Welcome to Next.js with Firebase!</h1>
+
+          <div className='row'>
+            {usuarios.map(usuario => (
+              <div className='card' id='data' key={usuario.id}>
+                <img src={usuario.avatar} alt='Avatar' className='avatar' />
+                <h3>{usuario.username}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <style jsx>{`
+          .avatar {
+            vertical-align: middle;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+          }
+          .hero {
+            width: 100%;
+            color: #333;
+          }
+          .title {
+            margin: 0;
+            width: 100%;
+            padding-top: 80px;
+            line-height: 1.15;
+            font-size: 48px;
+          }
+          .title,
+          .description {
+            text-align: center;
+          }
+          .row {
+            max-width: 880px;
+            margin: 80px auto 40px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+          }
+          .card {
+            padding: 18px 18px 24px;
+            width: 220px;
+            text-align: left;
+            text-decoration: none;
+            color: #434343;
+            border: 1px solid #9b9b9b;
+          }
+          .card:hover {
+            border-color: #067df7;
+          }
+          .card h3 {
+            margin: 0;
+            color: #067df7;
+            font-size: 18px;
+          }
+          .card p {
+            margin: 0;
+            padding: 12px 0 0;
+            font-size: 13px;
+            color: #333;
+          }
+        `}</style>
       </div>
-    </div>
+    );
+  }
+}
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Home;
